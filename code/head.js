5418133ad3545870662b2df86f4e6a74937be32f
Brad Loos <bradfordjamesloos@yahoo.com>
2020-04-19 21:34:58 -0700

    [Code Sub-Dir] Forgot to add / to end of base href

--- code/head.js
+++ code/head.js
@@ -1,5 +1,5 @@
 document.write(`
-	<base href="http://loosche.github.io" />
+	<base href="http://loosche.github.io/" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link rel="stylesheet" type="text/css" href="css/afarewelltocolor.css" />
 	<link rel="stylesheet" type="text/css" href="css/resume.css" />
