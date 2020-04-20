9a807d815090e606ba0ee020239066cc77515d3d
Brad Loos <bradfordjamesloos@yahoo.com>
2020-04-19 21:29:40 -0700

    [Added code sub-directory]
    Added the code sub-directory (and fixed up other pages to refer to
    it) so that I could have a single location to modify the header, bar
    and footer information.
    
    Also fixed a few typos in some documents.

---
+++ code/head.js
@@ -0,0 +1,7 @@
+document.write(`
+	<base href="c:/users/loos/Desktop/website/" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="css/afarewelltocolor.css" />
+	<link rel="stylesheet" type="text/css" href="css/resume.css" />
+	<title>Bradford James Los</title>`);
+
---
+++ code/includes.js
@@ -0,0 +1,15 @@
+document.getElementById("header").innerHTML= `
+	<h1>Bradford J. Loos</h1>
+	<h2>Computer Graphics Programmer @ <a href="https://bungie.net">Bungie</a></h2>
+	<div id="contact"></div>`;
+
+document.getElementById("bar").innerHTML= `
+	<div class="link"><a href="index.html">publications</a></div>
+	<div class="link"><a href="resume/resume.html">resume</a></div>
+	<div class="contactLinks">
+		<img src="images/msn.png" title="bradfordjamesloos on MSN"/>
+		<img src="images/yahoo.png" title="bradfordjamesloos on Yahoo"/>
+		<img src="images/twitter.png" title="loosche"/>
+	</div>`;
+
+document.getElementById("footer").innerHTML= `Original theme by <a href="http://www.bryantsmith.com">bryant smith</a>`;
