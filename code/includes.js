8f22811c76a8b02a24215ab6e60853a93b446b08
loosche <bradfordjamesloos@yahoo.com>
2023-09-05 20:46:28 -0700

    Added LinkedIn link to header bar

--- code/includes.js
+++ code/includes.js
@@ -1,6 +1,6 @@
 document.getElementById("header").innerHTML= `
 	<h1>Bradford J. Loos</h1>
-	<h2>Computer Graphics Programmer @ <a href="https://bungie.net">Bungie</a></h2>
+	<h2>Graphics Architect @ <a href="https://bungie.net">Bungie</a></h2>
 	<div id="contact"></div>`;
 
 document.getElementById("bar").innerHTML= `
@@ -10,6 +10,7 @@ document.getElementById("bar").innerHTML= `
 		<img src="images/msn.png" title="bradfordjamesloos on MSN"/>
 		<img src="images/yahoo.png" title="bradfordjamesloos on Yahoo"/>
 		<img src="images/twitter.png" title="loosche"/>
+		<a href="https://www.linkedin.com/in/brad-loos-1820a36/" style="margin:0"><img src="images/linkedin.png" title="Brad Loos on LinkedIn"/></a>
 	</div>`;
 
 document.getElementById("footer").innerHTML= `Original theme by <a href="http://www.bryantsmith.com">bryant smith</a>`;
